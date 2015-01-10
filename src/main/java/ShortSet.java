87fcd73b48f88acf8ee23e32de019563bd974d2e
Xiong Wen <Shawn@192-168-1-121.tpgi.com.au>
2015-01-11 09:08:19 +1100

    modification after adding of ShortSet

--- src/main/java/ShortSet.java
+++ src/main/java/ShortSet.java
@@ -1,5 +1,17 @@
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by Shawn on 10/01/15.
  */
 public class ShortSet {
+    public static void main(String[] args) {
+        Set<Short> s= new HashSet<Short>();
+        for (short i = 0; i < 100; i++) {
+            s.add(i);
+            s.remove(i - 1); // int-valued expression
+        }
+        System.out.println(s.size());
+
+    }
 }
