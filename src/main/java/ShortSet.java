770d551b80d2399533638bd01589cb0f32650d43
Xiong Wen <Shawn@192-168-1-121.tpgi.com.au>
2015-01-11 11:26:44 +1100

    added comment

--- src/main/java/ShortSet.java
+++ src/main/java/ShortSet.java
@@ -9,6 +9,7 @@ public class ShortSet {
         Set<Short> s= new HashSet<Short>();
         for (short i = 0; i < 100; i++) {
             s.add(i);
+            // this isn't actually removing, it can be seen from the declaration of the method
             s.remove(i - 1); // int-valued expression
         }
         System.out.println(s.size());
